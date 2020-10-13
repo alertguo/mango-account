d5fc6d50a6ee45acff8f84bcf4b825cb7439b1ec
alertguo <1196614612@qq.com>
2020-10-13 22:48:38 +0800

    create <Icon/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="nav">
     <router-link to="/money">
-      <svg>
-        <use xlink:href="#label"/>
-      </svg>
+      <Icon name="money"/>
       记账
     </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="statistics">统计</router-link>
+    <router-link to="statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg';
-
-console.log(x);
-
 export default {
-  name: 'Nav'
+  name: 'Nav',
 };
 </script>
 
