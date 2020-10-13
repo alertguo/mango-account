3a9ba3e851cb1ebf8882153243f263e99c1d1ea5
alertguo <1196614612@qq.com>
2020-10-13 19:04:19 +0800

    优化布局 Layout&slot

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">Statistics.vue</div>
-    <Nav/>
-  </div>
+  <Layout>
+    <p>Statistics.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -11,16 +10,3 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-  .nav-wrapper{
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-
-  }
-  .content{
-    overflow: auto;
-    flex-grow: 1;
-  }
-</style>
