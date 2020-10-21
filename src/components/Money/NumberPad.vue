9e22ca4bc215a1da5358c39f18e7abc7c85c6907
alertguo <1196614612@qq.com>
2020-10-21 16:07:41 +0800

    写NumberPad的CSS

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,8 +13,8 @@
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button>OK</button>
-      <button>0</button>
+      <button class="ok">OK</button>
+      <button class="zero">0</button>
       <button>.</button>
     </div>
   </div>
@@ -27,5 +27,51 @@
 </script>
 
 <style lang="scss" scoped>
-
+@import "~@/assets/style/helper.scss";
+.numberPad {
+  .output {
+    font-size: 24px;
+    font-family: Consolas,monospace;
+    padding: 18px 16px;
+    text-align: right;
+  }
+  .buttons {
+    button {
+      width: 25%;
+      height: 64px;
+      float: left;
+      background: transparent;
+      border: none;
+      &.ok {
+        height: 64px*2;
+        float: right;
+      }
+      &.zero {
+        width: 25%*2;
+      }
+      $bg: #f2f2f2;
+      &:nth-child(1){
+        background: $bg;
+      }
+      &:nth-child(2),&:nth-child(5){
+        background: darken($bg,4%);
+      }
+      &:nth-child(3),&:nth-child(6),&:nth-child(9){
+        background: darken($bg,4%*2);
+      }
+      &:nth-child(4),&:nth-child(7),&:nth-child(10){
+        background: darken($bg,4%*3);
+      }
+      &:nth-child(8),&:nth-child(11),&:nth-child(13){
+        background: darken($bg,4%*4);
+      }
+      &:nth-child(14){
+        background: darken($bg,4%*5);
+      }
+      &:nth-child(12){
+        background: darken($bg,4%*6);
+      }
+    }
+  }
+}
 </style>
