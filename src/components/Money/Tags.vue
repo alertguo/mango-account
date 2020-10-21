24e13ac85b42d72f95c403af73353ce16b96f368
alertguo <1196614612@qq.com>
2020-10-21 15:03:31 +0800

    写Tags的CSS

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="tags">
+    <div class="new"><button>新增标签</button></div>
     <ul class="current">
       <li>衣</li>
       <li>食</li>
       <li>住</li>
       <li>行</li>
     </ul>
-    <div class="new">新增标签</div>
   </div>
 </template>
 
@@ -17,5 +17,46 @@
 </script>
 
 <style lang="scss" scoped>
+.tags{
+  font-size: 14px;
+  padding: 16px;
+  //flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      border-radius: 50%;
+      padding: 1px 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
 
+//  > .new {
+//    padding-top: 16px;
+//    button {
+//      background: transparent;
+//      border: none;
+//      color: #999;
+//      border-bottom: 1px solid;
+//      padding: 0 4px;
+//    }
+//  }
+//}
 </style>
