64daa3f38417fcc250c1c456cd9da1b6ca86197f
alertguo <1196614612@qq.com>
2020-10-22 11:40:39 +0800

    @Prop 装饰器

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -9,11 +9,15 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-
+    @Prop(Number) readonly xxx: number | undefined;
+    // Prop 告诉 Vue , xxx 不是 data 数据，而是 prop
+    // Number 告诉 Vue , xxx 运行时是个 Number(Vue不支持小写的number)
+    // xxx 是属性名
+    // number | undefined 告诉 TS xxx 的编译时类型
 
   type = '-'; //'-'表示支出，'+'表示收入
   selectType(type: string) { // type 只能是 '-' 和 '+' 中的一个
@@ -22,7 +26,13 @@ export default class Types extends Vue {
     }
     this.type = type;
   }
-
+  mounted(){
+    if(this.xxx === undefined){
+      console.log('没有xxx');
+    }else{
+      console.log(this.xxx.toString());
+    }
+  }
 }
 // export default {
 //   name: 'types',
