f4b920d4aa09385b174d317e54103e017f1a996a
alertguo <1196614612@qq.com>
2020-10-24 20:46:43 +0800

    封装 model.ts

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number; // 上面的都是数据类型
+  createdAt?: Date; // 类 / 构造函数
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,23 +15,17 @@ import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
 
-const {model} = require('@/model.js');
-const recordList: Record[] = model.fetch();
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number; // 上面的都是数据类型
-  createdAt?: Date; // 类 / 构造函数
-}
+const recordList = model.fetch();
+
 @Component({
   components: {NumberPad, Types, Notes, Tags},
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = recordList;
-  record: Record = {
+  recordList = recordList;
+  record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0,
   };
 
@@ -44,7 +38,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
