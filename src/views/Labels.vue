385d19f17fbe61ca2872d51d44b21f46baa29133
alertguo <1196614612@qq.com>
2020-10-26 11:01:53 +0800

    封装 Button

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -8,11 +8,9 @@
         <Icon name="right"/>
       </router-link>
     </div>
-    <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">
-        新建标签
-      </button>
-    </div>
+    <Button @click.native="createTag">
+      新建标签
+    </Button>
   </Layout>
 </template>
 
@@ -20,9 +18,12 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
+import Button from '@/components/Button.vue';
 
 tagListModel.fetch();
-@Component
+@Component({
+  components: {Button}
+})
 export default class Labels extends Vue {
   tags = tagListModel.data;
 
@@ -59,21 +60,6 @@ export default class Labels extends Vue {
       color: #666666;
       margin-right: 16px;
     }
-  }
-}
-
-.createTag {
-  background: #767676;
-  color: #ffffff;
-  border-radius: 4px;
-  border: none;
-  height: 40px;
-  padding: 0 16px;
-
-  &-wrapper {
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
   }
 }
 </style>
