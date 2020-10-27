9357b84f65cdc920ced9064fa90bf141448cc5db
alertguo <1196614612@qq.com>
2020-10-27 16:26:47 +0800

    将 create 封装到recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,6 +1,6 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
-   data: [] as RecordItem[],
+  data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
@@ -8,6 +8,12 @@ const recordListModel = {
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     return this.data;
+  },
+  // 新增数据
+  create(record: RecordItem) {
+    const record2: RecordItem = recordListModel.clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   },
   // 保存数据
   save() {
