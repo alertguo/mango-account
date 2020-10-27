8e04344bd1184e00be27abfa373bb4d7bf13e327
alertguo <1196614612@qq.com>
2020-10-27 16:20:06 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,15 +1,17 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+   data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
   // 获取数据
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
   },
   // 保存数据
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 
