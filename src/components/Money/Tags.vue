5bbfd78c20df17a7a74af56a327920bd7295c2fe
alertguo <1196614612@qq.com>
2020-10-29 21:41:17 +0800

    Tags.vue 改为使用 get 获取数据

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -17,15 +17,13 @@ import {Component} from 'vue-property-decorator';
 import mixins from 'vue-class-component';
 import TagHelper from '@/mixins/TagHelper.ts';
 
-@Component({
-  computed: {
-    tagList() {
-      return this.$store.state.tagList;
-    }
-  }
-})
+@Component
 export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
+
+  get tagList() {
+    return this.$store.state.tagList;
+  }
 
   created() {
     this.$store.commit('fetchTag');
