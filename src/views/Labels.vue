d60a96bb8210eeeab75b470a33b04f8ae00013e2
alertguo <1196614612@qq.com>
2020-10-29 21:11:22 +0800

    抽离 createTag 放到 TagHelper.ts 作为mixins

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -15,9 +15,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper.ts';
 
 @Component({
   components: {Button},
@@ -27,16 +28,9 @@ import Button from '@/components/Button.vue';
     }
   }
 })
-export default class Labels extends Vue {
-
+export default class Labels extends mixins(TagHelper) {
   beforeCreate() {
     this.$store.commit('fetchTag');
-  }
-
-  createTag() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {return window.alert('标签名不能为空');}
-    this.$store.commit('createTag', name);
   }
 }
 </script>
