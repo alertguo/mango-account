0f33327f8f833465c163b274d7faea951abf9be5
alertguo <1196614612@qq.com>
2020-10-31 17:43:15 +0800

    初步使用v-deep给scoped的style加样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,31 @@
 <template>
   <Layout>
-    <p>Statistics.vue</p>
+    <Types class-prefix="xxx" :value.sync="yyy"/>
   </Layout>
 </template>
 
+<style lang="scss" scoped>
+::v-deep li {
+  &.selected {
+    background: white;
+
+    &::after {
+      display: none;
+    }
+  }
+}
+</style>
+
 <script lang="ts">
-export default {
-  name: 'Statistics',
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Types from '@/components/Money/Types.vue';
+
+@Component({
+  components: {Types}
+})
+export default class Statistics extends Vue {
+  yyy = '-';
+}
 </script>
 
