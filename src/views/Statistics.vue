6bb7662dd32256e97fab58c0220d7e5732254735
alertguo <1196614612@qq.com>
2020-11-01 14:40:44 +0800

    获取到 createAt

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -11,8 +11,49 @@
       <br/>
       interval: {{ interval }}
     </div>
+    <div>
+      <ol>
+        <li v-for="item in result" :key="item.id">{{ item }}</li>
+      </ol>
+    </div>
   </Layout>
 </template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Types from '@/components/Money/Types.vue';
+import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+import recordTypeList from '@/constants/recordTypeList';
+
+@Component({
+  components: {Tabs, Types}
+})
+export default class Statistics extends Vue {
+  type = '-';
+  interval = 'day';
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
+
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
+  get result() {
+    for (let i = 0; i < this.recordList.length; i++) {
+      console.log(this.recordList[i].createdAt);
+    }
+    return [];
+  }
+
+  created() {
+    this.$store.commit('fetchRecord');
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
 ::v-deep {
@@ -31,23 +72,3 @@
   }
 }
 </style>
-
-<script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue';
-import Tabs from '@/components/Tabs.vue';
-import intervalList from '@/constants/intervalList';
-import recordTypeList from '@/constants/recordTypeList';
-
-@Component({
-  components: {Tabs, Types}
-})
-export default class Statistics extends Vue {
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
-}
-</script>
-
