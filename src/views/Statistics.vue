aa7043e5db676854307c907b9c59b9b8b10d9823
alertguo <1196614612@qq.com>
2020-11-04 00:00:02 +0800

    修复空的收入无法点击的问题

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -48,10 +48,10 @@ export default class Statistics extends Vue {
 
   get groupedList() {
     const {recordList} = this;
-    if (recordList.length === 0) {return [];}
     const newList = clone(recordList)
       .filter(r => r.type === this.type)
       .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    if (newList.length === 0) {return [];}
     type Result = [{ title: string; total?: number; items: RecordItem[] }]
     const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
     for (let i = 1; i < newList.length; i++) {
