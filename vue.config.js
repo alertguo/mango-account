1be98013005e6d905c3f964b5067bf633a360cb9
alertguo <1196614612@qq.com>
2020-11-04 13:48:14 +0800

    部署到 mango-account-website

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mango-account-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
@@ -12,7 +15,7 @@ module.exports = {
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end() // 用于删除 svg 的 fill 属性
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugin: [{removeAttrs: {attrs: 'fill'}}]})).end()
-      //插件
+    //插件
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 dir 目录
   }
