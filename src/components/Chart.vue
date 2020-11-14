4348edf4bef0dcc5a1b528034c894af41d397356
alertguo <1196614612@qq.com>
2020-11-14 10:37:23 +0800

    监听 options ，变化时重新 setOptions

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -4,25 +4,32 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-import echarts, {EChartOption} from 'echarts';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+import echarts, {EChartOption, ECharts} from 'echarts';
 
 @Component
 export default class Chart extends Vue {
   @Prop() options?: EChartOption;
+  chart?: ECharts;
 
   mounted() {
-    if(this.options === undefined){
-      return console.log('options 为空')
+    if (this.options === undefined) {
+      return console.log('options 为空');
     }
-    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
-    chart.setOption(this.options);
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    this.chart.setOption(this.options);
   }
+
+  @Watch('options')
+  onOptionsChange(newValue: EChartOption) {
+    this.chart?.setOption(newValue);
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
-  .wrapper {
-    height: 400px;
-  }
+.wrapper {
+  height: 50vh;
+}
 </style>
